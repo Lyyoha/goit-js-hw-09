4ff9f8539b7c58e8945ebd8f933f461d7e67f8a0
Lyoha <gaystruk18@gmail.com>
2026-04-23 10:00:35 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import SortCss from 'postcss-sort-media-queries';
 
 export default defineConfig(({ command }) => {
   return {
-    base: '/goit-js-hw-09/',npm run build
+    base: '/goit-js-hw-09/',
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
